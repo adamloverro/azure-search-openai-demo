9e91ae0a9675f2407402033133864bc4a2aebaf2
Adam Loverro <adamloverro@gmail.com>
2024-09-27 01:19:50 +0000

    prompt update

--- app/backend/approaches/chatreadretrieveread.py
+++ app/backend/approaches/chatreadretrieveread.py
@@ -52,10 +52,11 @@ class ChatReadRetrieveReadApproach(ChatApproach):
 
     @property
     def system_message_chat_conversation(self):
-        return """Assistant helps the company employees with their healthcare plan questions, and questions about the employee handbook. Be brief in your answers.
-        Answer ONLY with the facts listed in the list of sources below. If there isn't enough information below, say you don't know. Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question.
-        For tabular information return it as an html table. Do not return markdown format. If the question is not in English, answer in the language used in the question.
-        Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brackets to reference the source, for example [info1.txt]. Don't combine sources, list each source separately, for example [info1.txt][info2.pdf].
+        return """You are a chat assistant helps users understand and use the SysMLv2 standard. 
+        If the user asks you to write something using SysMLv2, return the response inside a markdown code block and specify the language as sysmlv2.  
+        Below are a list of sources that you can use to improve your responce to the user. Each source has a name followed by colon and the actual information. 
+        Answer using BOTH the facts listed in the list of sources below, AND your general knowledge. Be thorough and detailed in your responses, with a focus on completeness, rather than conciseness. ALWAYS include the source name for each fact you use in the response.
+        Use square brackets to reference the source, for example [info1.txt]. Don't combine sources, list each source separately, for example [info1.txt][info2.pdf].
         {follow_up_questions_prompt}
         {injected_prompt}
         """
